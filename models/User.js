97034a7b6d4ba239d692e986eb7f010afb2372a6
msamm <msamm93@gmail.com>
2022-05-17 18:50:24 -0500

    initial files and user api routes

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // password must be at least 4 characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS GO HERE
+
+        // pass in our imported sequelize connection to the database
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp field
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // user underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
