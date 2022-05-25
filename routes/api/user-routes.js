9c8e61ae0e73a199c6aefb07bf18b59cc2a462b8
msamm <msamm93@gmail.com>
2022-05-25 17:01:01 -0500

    voting system

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { disable } = require('express/lib/application');
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll method (equivalent of SELECT * FROM users)
     User.findAll({
-        attributes: { exclude: ['password'] }
+        attributes: { exclude: ['password'] },
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -19,6 +18,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ],
         where: {
             id: req.params.id
         }
